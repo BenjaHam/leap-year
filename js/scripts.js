d23ad610a4863f303ba0a9e6f0328056b936ed4d
Benjamin Hampikian and Dylan Lewis <dylan@>
2017-06-07 08:56:36 -0700

    add second spec to back end logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
   $("form#leap-year").submit(function(event) {
     event.preventDefault();
     var leapYear = function(year) {
+    if (year % 4 === 0) {
+      return true;
+    } else {
       return false;
-    };
+    }
+  };
 
 
     var year = parseInt($("input#year").val());
